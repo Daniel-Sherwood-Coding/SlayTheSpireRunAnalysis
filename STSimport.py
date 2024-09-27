d04e1a815b79ba8088c34131e9d5dca7c50d54c9
Daniel-Sherwood-Coding <danielsherwood2014@gmail.com>
2024-09-27 18:50:16 +0100

    initial_commit

---
+++ STSimport.py
@@ -0,0 +1,6 @@
+import json
+import os
+
+if __name__ == __main__:
+    with open(os.path.join(r"E:\SteamLibrary\steamapps\common\SlayTheSpire\runs\IRONCLAD\1627949176.run"), "r") as file:
+        Dataset = json.load(file)
